9768f49ec321a5f9d0307fbc02a52895aa61a0e3
Arman Patel <vanixgaming634@gmail.com>
2025-05-05 12:52:44 +0530

    feat: implement AddInstructorButton component with dialog for adding instructors; add Skeleton component for loading states

--- app/(roles)/admin/courses/_components/add-instructor-button.tsx
+++ app/(roles)/admin/courses/_components/add-instructor-button.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+'use client'
+import { Button } from "@/components/ui/button";
+import {
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Dialog, DialogDescription } from "@radix-ui/react-dialog";
+import { Plus } from "lucide-react";
+import React, { useState } from "react";
 
 const AddInstructorButton = () => {
-  return (
-    <div>AddInstructorButton</div>
-  )
-}
+  const[email,setEmail] = useState('')
+  const [loading,setLoading] = useState(false)
+  const [found,setFound ] = useState(null)
+  const [error,setError] = useState(null)
 
-export default AddInstructorButton
+  return (
+    
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          size="sm"
+          className="flex items-center gap-2 cursor-pointer bg-violet-600 hover:bg-violet-700 text-white shadow-sm"
+        >
+          <Plus size={16} /> Add Instructor
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add Instructor</DialogTitle>
+          <DialogDescription>Add Instructors by using email</DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col gap-4">
+          
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddInstructorButton;

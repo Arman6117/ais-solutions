6798e2c3457edcc11f43165e315725d546af3d38
Arman Patel <vanixgaming634@gmail.com>
2025-06-02 13:53:57 +0530

    feat: adjust styling of LectureSchedule component for improved readability

--- app/(roles)/student/dashboard/_components/lecture-schedule.tsx
+++ app/(roles)/student/dashboard/_components/lecture-schedule.tsx
@@ -6,8 +6,8 @@ const LectureSchedule = () => {
   return (
     <div className='flex flex-col gap-7'>
      <div className='flex items-center gap-2'>
-        <Calendar className='text-primary-bg size-8'/>
-        <h1 className='text-3xl font-bold'>Your Schedule</h1>
+        <Calendar className='text-primary-bg size-7'/>
+        <h1 className='text-2xl font-semibold'>Your Schedule</h1>
      </div>
      <div className=' px-5 py-2 w-fit rounded-md '>
         <LectureScheduleTab/>

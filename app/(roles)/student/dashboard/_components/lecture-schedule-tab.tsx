45202d956a4b6e07cb93afd132dacd63572a2789
Arman Patel <vanixgaming634@gmail.com>
2025-06-02 13:51:52 +0530

    feat: update LectureScheduleTab to include additional content for tomorrow's schedule

--- app/(roles)/student/dashboard/_components/lecture-schedule-tab.tsx
+++ app/(roles)/student/dashboard/_components/lecture-schedule-tab.tsx
@@ -5,7 +5,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 
 const LectureScheduleTab = () => {
   return (
-    <Tabs  defaultValue="today" orientation="horizontal">
+    <Tabs defaultValue="today" orientation="horizontal">
       <TabsList className="space-x-11 sm:p-7 bg-primary-bg/10 shadow">
         <TabsTrigger className="text-lg sm:p-5 cursor-pointer " value="today">
           Today
@@ -20,8 +20,13 @@ const LectureScheduleTab = () => {
           <LectureScheduleCard day="today" />
           <LectureScheduleCard day="today" />
         </TabsContent>
+
+        <TabsContent value="tomorrow" className="flex flex-col gap-6 p-3">
+          <LectureScheduleCard day="today" />
+          <LectureScheduleCard day="today" />
+          <LectureScheduleCard day="today" />
+        </TabsContent>
       </ScrollArea>
-      <TabsContent value="tomorrow">Hello</TabsContent>
     </Tabs>
   );
 };

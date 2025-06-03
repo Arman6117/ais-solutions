72ea2e01bd04347fdf58aaaf055cd95434b000bf
Arman Patel <vanixgaming634@gmail.com>
2025-06-03 13:13:28 +0530

    feat: make StudentBatchesCard clickable with navigation to batch details; improve styling and layout

--- app/(roles)/student/dashboard/_components/student-batches-card.tsx
+++ app/(roles)/student/dashboard/_components/student-batches-card.tsx
@@ -2,6 +2,7 @@ import { DummyBatches } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { BookOpen, Calendar, CalendarCheckIcon, Clock } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { FaChalkboardTeacher } from "react-icons/fa";
 import { PiChalkboardTeacher } from "react-icons/pi";
@@ -12,18 +13,19 @@ type StudentBatchesCardProps = {
 };
 const StudentBatchesCard = ({ batch, status }: StudentBatchesCardProps) => {
   return (
-    <div
+    <Link
+      href={`/student/batch-details/${batch.id}`}
       className={cn(
-        "flex rounded transition-all p-4 gap-6",
+        "flex rounded cursor-pointer transition-all p-4 gap-6",
         status === "Ongoing" &&
-          "bg-green-200/70 shadow-lg hover:outline-green-500 hover:outline-2 hover:shadow-lg hover:shadow-green-200",
+          "bg-green-200 shadow-lg hover:outline-green-500 hover:outline-2 hover:shadow-lg hover:shadow-green-200",
         status === "Upcoming" &&
-          "bg-blue-200/70 hover:outline-blue-500 hover:outline-2 hover:shadow-lg hover:shadow-blue-200",
+          "bg-indigo-200 hover:outline-blue-500 hover:outline-2 hover:shadow-lg hover:shadow-blue-200",
         status === "Completed" &&
-          "bg-amber-200/70 hover:outline-amber-500 hover:outline-2 hover:shadow-lg hover:shadow-amber-200"
+          "bg-amber-200 hover:outline-amber-500 hover:outline-2 hover:shadow-lg hover:shadow-amber-200"
       )}
     >
-      <div className="bg-soft-white rounded-lg size-20 p-1">
+      <div className="bg-white rounded-lg size-20 p-1">
         <Image
           alt="course thumbnail"
           className="rounded-lg"
@@ -33,36 +35,35 @@ const StudentBatchesCard = ({ batch, status }: StudentBatchesCardProps) => {
         />
       </div>
       <div className="flex flex-col gap-2">
-        <h1 className="text-lg font-medium">{batch.name}</h1>
+        <h1 className="text-lg font-medium ">{batch.name}</h1>
         <div className="flex gap-2 items-center text-neutral-600">
           <Clock className="size-4" />
-          <span className="text-sm font-medium">{batch.time}</span>
+          <span className="text-sm ">{batch.time}</span>
         </div>
         <div className="flex gap-6 mt-2">
           <div className="flex gap-2 items-center text-neutral-600">
             <BookOpen className="size-4" />
-            <span className="text-sm font-medium">10 Modules</span>
+            <span className="text-sm ">10 Modules</span>
           </div>
           <div className="flex gap-2 items-center text-neutral-600">
             <FaChalkboardTeacher className="size-4" />
-            <span className="text-sm font-medium">John Doe</span>
+            <span className="text-sm ">John Doe</span>
           </div>
         </div>
-        {status ==='Upcoming' && (
-
+        {status === "Upcoming" && (
           <div className="flex gap-6 mt-2">
-          <div className="flex gap-2 items-center text-neutral-600">
-            <Calendar className="size-4" />
-            <span className="text-sm font-medium">{batch.startDate}</span>
+            <div className="flex gap-2 items-center text-neutral-600">
+              <Calendar className="size-4" />
+              <span className="text-sm ">{batch.startDate}</span>
+            </div>
+            <div className="flex gap-2 items-center text-neutral-600">
+              <CalendarCheckIcon className="size-4" />
+              <span className="text-sm font">{batch.endDate}</span>
+            </div>
           </div>
-          <div className="flex gap-2 items-center text-neutral-600">
-            <CalendarCheckIcon className="size-4" />
-            <span className="text-sm font-medium">{batch.endDate}</span>
-          </div>
-        </div>
         )}
       </div>
-    </div>
+    </Link>
   );
 };
 

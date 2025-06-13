02c693a6e7764b5c8554e6a7674d7526cb0aabaf
Arman Patel <vanixgaming634@gmail.com>
2025-06-14 00:35:34 +0530

    feat: add Zustand for state management; implement CourseSelector component and course store for course selection

---
+++ app/(roles)/student/dashboard/_components/course-selector.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseSelector = () => {
+  return (
+    <div>CourseSelector</div>
+  )
+}
+
+export default CourseSelector
---
+++ store/use-course-store.ts
@@ -0,0 +1,20 @@
+import { Course } from "@/lib/types";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type CourseStore = {
+  selectedCourse: Course | null;
+  setSelectedCourse: (course: Course) => void;
+};
+
+export const useCourseStore = create<CourseStore>()(
+  persist(
+    (set) => ({
+      selectedCourse: null,
+      setSelectedCourse: (course) => set({ selectedCourse: course }),
+    }),
+    {
+      name: "course-store",
+    }
+  )
+);

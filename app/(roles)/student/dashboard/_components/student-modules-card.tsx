d2a987746228921276f0e333178e1bcea9ce579c
Arman Patel <vanixgaming634@gmail.com>
2025-06-15 14:23:15 +0530

    feat: implement StudentModulesMeetingDialog component for session history; refactor StudentModulesCard to utilize the new dialog and improve module display

--- app/(roles)/student/dashboard/_components/student-modules-card.tsx
+++ app/(roles)/student/dashboard/_components/student-modules-card.tsx
@@ -1,69 +1,73 @@
-'use client'
+"use client";
 import { DummyModules } from "@/lib/types";
-import { cn, generateReadableLightColor, getLevelColor, getStatusColor } from "@/lib/utils";
+import { cn, getStatusColor } from "@/lib/utils";
 import { BookOpen, Calendar, CalendarCheckIcon, Clock } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useMemo, useState } from "react";
 import { FaChalkboardTeacher } from "react-icons/fa";
+import StudentModulesMeetingDialog from "./student-modules-meeting-dialog";
+import { dummySessions } from "@/lib/static";
 
 type StudentModulesCardProps = {
   module: DummyModules;
   status: "Ongoing" | "Upcoming" | "Completed";
 };
 const StudentModulesCard = ({ module, status }: StudentModulesCardProps) => {
-
-   const bgStyle = useMemo(()=> {
-    return getStatusColor(module.status)
-   },[module.id])
+  const bgStyle = useMemo(() => {
+    return getStatusColor(module.status);
+  }, [module.id]);
 
   //  console.log(bgStyle)
   return (
-    <Link
-      href={`/student/module-details/${module.id}`}
-      className={cn(
-        "flex rounded cursor-pointer border-2 transition-all p-4 gap-6",
-        `hover:shadow-lg hover:shadow-primary-bg/10 hover:border-2 hover:border-primary-bg`,
-      
-      bgStyle.bg ,bgStyle.border,bgStyle.text)}
-    
-    >
-      <div className="bg-white rounded-lg size-20 p-1">
-        <Image
-          alt="course thumbnail"
-          className="rounded-lg"
-          src={"https://placehold.co/80x80"}
-          width={80}
-          height={80}
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <h1 className="text-lg font-medium ">{module.name}</h1>
-      
-        <div className="flex gap-6 mt-2">
-          <div className="flex gap-2 items-center text-neutral-600">
-            <BookOpen className="size-4" />
-            <span className="text-sm ">10 Chapter</span>
-          </div>
-          <div className="flex gap-2 items-center text-neutral-600">
-            <FaChalkboardTeacher className="size-4" />
-            <span className="text-sm ">John Doe</span>
-          </div>
+    <StudentModulesMeetingDialog session={dummySessions} >
+      <div
+        className={cn(
+          "flex rounded  cursor-pointer border-2 transition-all p-4 gap-6",
+          `hover:shadow-lg hover:shadow-primary-bg/10 hover:border-2 hover:border-primary-bg`,
+
+          bgStyle.bg,
+          bgStyle.border,
+          bgStyle.text
+        )}
+      >
+        <div className="bg-white rounded-lg size-20 p-1">
+          <Image
+            alt="course thumbnail"
+            className="rounded-lg"
+            src={"https://placehold.co/80x80"}
+            width={80}
+            height={80}
+          />
         </div>
-        {status === "Upcoming" && (
+        <div className="flex flex-col gap-2">
+          <h1 className="text-lg font-medium ">{module.name}</h1>
+
           <div className="flex gap-6 mt-2">
             <div className="flex gap-2 items-center text-neutral-600">
-              <Calendar className="size-4" />
-              <span className="text-sm ">20 June 2025</span>
+              <BookOpen className="size-4" />
+              <span className="text-sm ">10 Chapter</span>
             </div>
             <div className="flex gap-2 items-center text-neutral-600">
-              <CalendarCheckIcon className="size-4" />
-              <span className="text-sm font">20 July 2025</span>
+              <FaChalkboardTeacher className="size-4" />
+              <span className="text-sm ">John Doe</span>
             </div>
           </div>
-        )}
+          {status === "Upcoming" && (
+            <div className="flex gap-6 mt-2">
+              <div className="flex gap-2 items-center text-neutral-600">
+                <Calendar className="size-4" />
+                <span className="text-sm ">20 June 2025</span>
+              </div>
+              <div className="flex gap-2 items-center text-neutral-600">
+                <CalendarCheckIcon className="size-4" />
+                <span className="text-sm font">20 July 2025</span>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </Link>
+    </StudentModulesMeetingDialog>
   );
 };
 
---
+++ app/(roles)/student/dashboard/_components/student-modules-meeting-dialog.tsx
@@ -0,0 +1,42 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { dummySessions } from "@/lib/static";
+import React from "react";
+import SessionCard from "../../sessions/_components/session-card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+type StudentModulesMeetingDialogProps = {
+  children: React.ReactNode;
+  session: (typeof dummySessions)[0][];
+};
+const StudentModulesMeetingDialog = ({
+  children,
+  session,
+}: StudentModulesMeetingDialogProps) => {
+  return (
+    <Dialog >
+      <DialogTrigger className="w-full ">{children}</DialogTrigger>
+      <DialogContent className="max-w-3xl! ">
+        <DialogHeader>
+          <DialogTitle>Module Name Session History</DialogTitle>
+        </DialogHeader>
+        <ScrollArea className="flex max-h-[500px] flex-col gap-3">
+          {session.map((s) => (
+            <div key={s.id} className="mb-3" >
+
+            <SessionCard session={s} />
+            </div>
+          ))}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default StudentModulesMeetingDialog;

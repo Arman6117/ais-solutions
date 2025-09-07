cb3fce0955ae5992b532bb8ab0ab1d074889bc80
Arman Patel <vanixgaming634@gmail.com>
2025-09-08 02:35:25 +0530

    fix:base url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://ais-solutions-3z4mvo7pc-arman6117s-projects.vercel.app"
+    baseURL: "https://ais-solutions.vercel.app/"
 })

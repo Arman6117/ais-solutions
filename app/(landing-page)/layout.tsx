da1c597c901ac34c8839c11418682ff5f1767118
Arman Patel <vanixgaming634@gmail.com>
2025-09-08 19:40:19 +0530

    feat:changed landing page

--- app/(landing-page)/layout.tsx
+++ app/(landing-page)/layout.tsx
@@ -1,10 +1,10 @@
-import Navbar from '@/components/navbar'
+// import Navbar from '@/components/navbar'
 import React from 'react'
 
 const HomePageLayout = ({children}:{children:React.ReactNode}) => {
   return (
     <main className='py-4 px-7'>
-    <Navbar/>
+    {/* <Navbar/> */}
     {children}
     </main>
   )

7dd749bb6d08f61329d71ecc0a42bd78e7225905
Arman Patel <vanixgaming634@gmail.com>
2025-10-01 16:01:49 +0530

    fix

--- components/student-course-component/student-course-details.tsx
+++ components/student-course-component/student-course-details.tsx
@@ -147,6 +147,7 @@ const StudentCourseDetails = ({
             </Dialog>
           </CardContent>
         </Card>
+       
         {/* <StudentCourseReviews className="flex md:hidden" isEnrolled={false} /> */}
       </div>
     </div>

97e6b933fcfc6c4e80359e8caf35029a7571f4c8
Arman Patel <vanixgaming634@gmail.com>
2025-11-18 16:34:17 +0530

    changed color

--- app/(roles)/student/dashboard/_components/student-group-join-button.tsx
+++ app/(roles)/student/dashboard/_components/student-group-join-button.tsx
@@ -6,8 +6,8 @@ export const revalidate = 60;
 const StudentGroupJoinButton = ({ groupLink }: { groupLink: string }) => {
   return (
     <Link href={groupLink}>
-      <Button className="bg-soft-white cursor-pointer hover:bg-indigo-200 flex gap-1 max-w-16  items-center rounded-md shado-md p2 ">
-        <FaWhatsapp className="text-primary-bg size-5" />
+      <Button className="bg-green-700 cursor-pointer hover:bg-soft-white flex gap-1 max-w-16  items-center rounded-md shado-md p2 ">
+        <FaWhatsapp className="text-white hover:text-green-600 size-5" />
       </Button>
     </Link>
   );

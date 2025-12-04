cab00098fa81b0fc4011223c76f69fc7a347570a
Arman Patel <vanixgaming634@gmail.com>
2025-12-05 00:07:59 +0530

    removed unused code

--- app/auth/login/instructor/page.tsx
+++ app/auth/login/instructor/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import StudentLoginForm from '../student/_components/student-login-form'
 import InstructorLoginForm from './_components/instructor-login-form'
 
 const InstructorLoginPage = () => {
